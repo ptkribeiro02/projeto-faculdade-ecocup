4bedad9f19c0ce9372de654d63c27b1ec9cee4b4
bradock <patrickribeiro22360@gmail.com>
2025-06-05 17:30:56 -0300

    frist commit

---
+++ home/script.js
@@ -0,0 +1,10 @@
+function toggleTheme() {
+  const body = document.body;
+  if (body.classList.contains('light-mode')) {
+    body.classList.remove('light-mode');
+    body.classList.add('dark-mode');
+  } else {
+    body.classList.remove('dark-mode');
+    body.classList.add('light-mode');
+  }
+}
